81cba4517fae1e0dc1b0b30b4f7505ce58636964
JoseDv1 <jose123millon@gmail.com>
2025-07-03 10:18:55 -0500

    feat: actualizar esquema de usuario para omitir contraseña y agregar hashed_password, y modificar la configuración de Prisma

--- apps/api/src/db/prisma.ts
+++ apps/api/src/db/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../../../../packages/db/generated/prisma/client";
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+	omit: {
+		user: {
+			hashedPassword: true, // Omit hashedPassword field by default
+		}
+	}
+})
