e40c2d46a07d4a8c185e71dd3faa54da37ae4ea1
JoseDv1 <jose123millon@gmail.com>
2025-07-03 12:55:01 -0500

    feat: eliminar columnas obsoletas de la tabla accounts y hacer que el campo role en users sea obligatorio

---
+++ packages/db/prisma/migrations/20250703162115_role/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `scope` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `accounts` table. All the data in the column will be lost.
+  - Made the column `role` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "accounts" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "refresh_token",
+DROP COLUMN "scope",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type";
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "role" SET NOT NULL;
