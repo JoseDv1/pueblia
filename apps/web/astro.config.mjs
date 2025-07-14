48e9147e551e83777780b0ab027f12f7caa9b490
JoseDv1 <jose123millon@gmail.com>
2025-07-14 12:33:58 -0500

    feat: reestructurar integración de la barra de herramientas y restaurar funcionalidad de cambio de esquema de color

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import svelte from '@astrojs/svelte';
-import alpinejs from '@astrojs/alpinejs';
-import themeToggler from "./toolbar-app/integration.ts";
+import svelte from "@astrojs/svelte";
+import alpinejs from "@astrojs/alpinejs";
+import themeToggler from "../../packages/toolbar-app/integration.ts";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), alpinejs(), themeToggler]
+	integrations: [svelte(), alpinejs(), themeToggler],
 });
